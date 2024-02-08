804434c5002ce923a773a87cd8a843df14a49582
Sergey Kosovsky <skosovsky@gmail.com>
2024-02-08 19:42:05 +0300

    Add test Dockerfile

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
-for i in 1 2 3 4 5; do
-  echo $i
-done
+name=$1
+len=${#name}
+first_letter=${name::1}
+rest_letters=${name:1:len}
+if [ -z "$name" ]; then
+    echo "Нужно ввести аргумент"
+    exit 1
+else
+  echo "Hello, ${first_letter^}${rest_letters,,} :)"
+fi
